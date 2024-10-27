cee96444aeaed937c752e33897010b41fc202819
Mouad Labrirhil <mlabrirh@c3r9p9.1337.ma>
2024-10-27 13:03:47 +0100

    hello

---
+++ ft_strnstr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 11:10:08 by mlabrirh          #+#    #+#             */
+/*   Updated: 2024/10/27 11:58:54 by mlabrirh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	j = ft_strlen(little);
+
+	if (little[0] == 0)
+	{
+		return ((char *)big);
+	}
+	while (big[i] != 0 && len-- >= j)
+	{
+		if (ft_strncmp(&big[i], little, j) == 0)
+		{
+			return ((char *)&big[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
+int main ()
+{
+    char s[] = "hello word welcom to 1337";
+    char c[] = "1337";
+    char *result = ft_strnstr(s,c,20);
+    printf("%s\n",result);
+}
