71b48579543410ecbd416367dfa78a997b042e57
Aspegic01 <mlabrirh@e1r13p4.1337.ma>
2024-10-28 18:35:57 +0100

    j

---
+++ ft_strjoin.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 18:10:39 by mlabrirh          #+#    #+#             */
+/*   Updated: 2024/10/28 18:23:54 by mlabrirh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len;
+	char	*str;
+	int	i;
+	int	j;
+	
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return str;
+
+}
+int main()
+{
+	char s1[] = "hello";
+	char s2[] = " word";
+	char *r = ft_strjoin(s1,s2);
+	puts(r);
+}
