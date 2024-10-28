65ec7625b2dd88d4675daad2b5c52e36fd9de166
Aspegic01 <mlabrirh@e1r13p4.1337.ma>
2024-10-28 13:53:09 +0100

    h

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,20 +6,24 @@
 /*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 10:43:53 by mlabrirh          #+#    #+#             */
-/*   Updated: 2024/10/28 10:59:39 by mlabrirh         ###   ########.fr       */
+/*   Updated: 2024/10/28 12:29:38 by mlabrirh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	unsigned char *tmp;
-	
+	size_t	i;
+
 	tmp = malloc(count * size);
 	
 	if (!tmp)
 	{
 		return (NULL);
 	}
+	i = 0;
 	
 	while (i < count * size)
 	{
--- libft.h
+++ libft.h
@@ -1,6 +1,7 @@
 #ifndef LIBFT_H
 #define LIBFT_H
 #include <stdio.h>
+#include <stdlib.h>
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
