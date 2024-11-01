720135d7c849350b034898ae561228e934e5162e
Aspegic01 <mlabrirh@e2r6p9.1337.ma>
2024-11-01 14:33:50 +0100

    l

--- f.c
+++ f.c
@@ -1,73 +1,65 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   c.c                                                :+:      :+:    :+:   */
+/*   f.c                                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 11:44:35 by mlabrirh          #+#    #+#             */
-/*   Updated: 2024/11/01 14:29:09 by mlabrirh         ###   ########.fr       */
+/*   Created: 2024/11/01 09:47:35 by mlabrirh          #+#    #+#             */
+/*   Updated: 2024/11/01 11:42:13 by mlabrirh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int glen(int n)
+int glen(int n) 
 {
     int len = 0;
     if (n == 0) 
-    {
+	{
         return 1; 
     }
-    if (n < 0)
-    { 
+    if (n < 0) 
+	{ 
         n *= -1;
-        len++;
+		len++;
     }
-    while (n > 0)
-    {
+    while (n > 0) 
+	{
         n = n / 10;
         len++;
     }
     return len;
 }
-
-void fill_string(char *str, int n, int len)
-{
-    while (n > 0)
-    {
-        str[--len] = n % 10 + '0';
-        n  = n / 10;
-        
-    }
-}
-
-char *ft_itoa(int n)
+char *ft_itoa(int n) 
 {
     int len = glen(n);
-    char *str = (char *)malloc(sizeof(char) * (len + 1));
+    char *str = (char *)malloc(sizeof(char) * (len + 1)); 
     if (str == 0)
-        return 0;
+        return 0; 
     if (n == 0)
-    {
+	{
         str[0] = '0';
-        str[1] = '\0';
-        return str;
-    }
+        str[1] = '\0'; 
+   		return str;
+	}
     int is_negative = 0;
     if (n < 0)
-    {
+	{
         is_negative = 1;
         n *= -1;
     }
-    str[len] = '\0';
-    fill_string(str, n, len);
-    if (is_negative)
+    str[len] = '\0'; 
+    while (n > 0)
+	{
+        str[--len] = n % 10 + '0'; 
+        n /= 10;
+	}
+    if (is_negative) 
+	{
         str[0] = '-';
+    }
     return str;
 }
-int main()
-{
-    printf("%s\n",ft_itoa(-892182));
-}
+
--- ft_itoa.c
+++ ft_itoa.c
@@ -5,13 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/30 17:19:15 by mlabrirh          #+#    #+#             */
-/*   Updated: 2024/10/30 17:22:05 by mlabrirh         ###   ########.fr       */
+/*   Created: 2024/11/01 09:04:41 by mlabrirh          #+#    #+#             */
+/*   Updated: 2024/11/01 09:24:00 by mlabrirh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char	*ft_itoa(int n)
+
+size_t	ft_getlen(long n)  // static keyword removed
 {
-	
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+	{
+		i++;
+		n = -n;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_itoa(int m)
+{
+	size_t	len;
+	long	n;
+	char	*s;
+
+	n = (long)m;
+	len = ft_getlen(n);
+	s = (char *)malloc(sizeof(char) * len + 1);
+	if (!s)
+		return (NULL);
+	if (!n)
+		s[0] = '0';
+	if (n < 0)
+	{
+		s[0] = '-';
+		n = -n;
+	}
+	s[len] = 0;
+	while (n > 0)
+	{
+		s[--len] = n % 10 + 48;
+		n /= 10;
+	}
+	return (s);
+}
+#include "libft.h" // Assuming this contains the declaration of ft_itoa
+#include <stdio.h>
+
+int main() {
+  printf("%s\n",ft_itoa(37463));
+
+  return 0;
 }
