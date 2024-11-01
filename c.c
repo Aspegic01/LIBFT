2ec104cc20b5b71e91251f67678d1f6e0b439997
Aspegic01 <mlabrirh@e2r6p9.1337.ma>
2024-11-01 16:21:18 +0100

    hello

--- c.c
+++ c.c
@@ -6,68 +6,62 @@
 /*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 11:44:35 by mlabrirh          #+#    #+#             */
-/*   Updated: 2024/11/01 14:29:09 by mlabrirh         ###   ########.fr       */
+/*   Updated: 2024/11/01 16:17:18 by mlabrirh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int glen(int n)
+int count_digit(int n)
 {
-    int len = 0;
-    if (n == 0) 
-    {
-        return 1; 
-    }
+    int len;
+
+    len = 1;
     if (n < 0)
     { 
         n *= -1;
         len++;
     }
-    while (n > 0)
+    while (n > 9)
     {
         n = n / 10;
         len++;
     }
     return len;
 }
-
-void fill_string(char *str, int n, int len)
+void ft_strrev(char *str, int n, int len)
 {
     while (n > 0)
     {
         str[--len] = n % 10 + '0';
         n  = n / 10;
-        
     }
 }
-
 char *ft_itoa(int n)
 {
-    int len = glen(n);
+    int len;
+    int is_negative;
+
+    len = count_digit(n);
     char *str = (char *)malloc(sizeof(char) * (len + 1));
     if (str == 0)
         return 0;
-    if (n == 0)
-    {
+    if(!n)
         str[0] = '0';
-        str[1] = '\0';
-        return str;
-    }
-    int is_negative = 0;
+    is_negative = 0;
     if (n < 0)
     {
         is_negative = 1;
         n *= -1;
     }
     str[len] = '\0';
-    fill_string(str, n, len);
+    ft_strrev(str, n, len);
     if (is_negative)
         str[0] = '-';
     return str;
 }
 int main()
 {
-    printf("%s\n",ft_itoa(-892182));
+    printf("%s\n",ft_itoa(0));
 }
--- f.c
+++ f.c
@@ -5,61 +5,62 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mlabrirh <mlabrirh@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 09:47:35 by mlabrirh          #+#    #+#             */
-/*   Updated: 2024/11/01 11:42:13 by mlabrirh         ###   ########.fr       */
+/*   Created: 2024/11/01 15:48:03 by mlabrirh          #+#    #+#             */
+/*   Updated: 2024/11/01 16:03:02 by mlabrirh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include <stdio.h>
-#include <stdlib.h>
+#include "libft.h"
 
-int glen(int n) 
+int	ft_getlen(int n)
 {
-    int len = 0;
-    if (n == 0) 
+	int	i;
+
+	i = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
 	{
-        return 1; 
-    }
-    if (n < 0) 
-	{ 
-        n *= -1;
-		len++;
-    }
-    while (n > 0) 
-	{
-        n = n / 10;
-        len++;
-    }
-    return len;
-}
-char *ft_itoa(int n) 
-{
-    int len = glen(n);
-    char *str = (char *)malloc(sizeof(char) * (len + 1)); 
-    if (str == 0)
-        return 0; 
-    if (n == 0)
-	{
-        str[0] = '0';
-        str[1] = '\0'; 
-   		return str;
+		i++;
+		n = -n;
 	}
-    int is_negative = 0;
-    if (n < 0)
+	while (n > 0)
 	{
-        is_negative = 1;
-        n *= -1;
-    }
-    str[len] = '\0'; 
-    while (n > 0)
-	{
-        str[--len] = n % 10 + '0'; 
-        n /= 10;
+		n /= 10;
+		i++;
 	}
-    if (is_negative) 
-	{
-        str[0] = '-';
-    }
-    return str;
+	return (i);
 }
 
+char	*ft_itoa(int m)
+{
+	size_t	len;
+	long	n;
+	char	*s;
+
+	n = (long)m;
+	len = ft_getlen(n);
+	s = (char *)malloc(sizeof(char) * len + 1);
+	if (!s)
+		return (NULL);
+	if (!n)
+		s[0] = '0';
+	if (n < 0)
+	{
+		s[0] = '-';
+		n = -n;
+	}
+	s[len] = 0;
+	while (n > 0)
+	{
+		s[--len] = n % 10 + 48;
+		n /= 10;
+	}
+	return (s);
+}
+int main()
+{
+    printf("%s\n",ft_itoa(0));
+}
